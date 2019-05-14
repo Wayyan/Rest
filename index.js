c4a9973de43af4a1e6f9a656d7b90b46743c436f
Wayyan <46549417+Wayyan@users.noreply.github.com>
2019-05-14 15:06:25 +0630

    Start

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const http=require('http');
+const fs=require('fs');
+var routers={
+    '/base':function index(req,res){
+        const readStream=fs.createReadStream('./data.json');
+        res.writeHead(200,{'Content-type':'application/json'});
+        readStream.pipe(res);
+    },
+};
+const server=http.createServer((req,res)=>{
+    if(req.url in routers){
+        return routers[req.url](req,res);
+    }
+    res.writeHead(404);
+    res.end('Not Found');
+});
+// const express=require('express');
+// const app=express();
+// app.get('/patch',(req,res)=>{
+//  res.send("1");
+// });
+server.listen(process.env.PORT||'3000');
