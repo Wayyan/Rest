acf438032608bc912a4c2f9ed20590cc904587b4
Wayyan <46549417+Wayyan@users.noreply.github.com>
2019-06-05 11:20:58 +0630

    add

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -3,22 +3,28 @@ var mongo = require('mongodb');
 var file = require('../../data.json');
 const fs = require('fs');
 var MongoClient = mongo.MongoClient;
-var url = process.env.MONGODB_URL||"mongodb://localhost:27017/";
+var url = process.env.MONGODB_URL || "mongodb://localhost:27017/";
 const client = new MongoClient(url, { useNewUrlParser: true });
+
 exports.write_all_data = function (req, res) {
     var json = file;
-    client.connect(function (err) {
+    // client.connect(function (err) {
+    //     if (err) throw err;
+    //     var dbo = client.db("mydb");
+    //     dbo.collection("chords").find({}, { projection: { _id: 0, name: 1, artis: 2, album: 3, type: 4, link: 5 } }).toArray(function (err, result) {
+    //         if (err) throw err;
+    //         result.forEach(function (result) {
+    //             json['Sheet1'].push(result);
+    //         });
+    //         res.json(json);
+    //         client.close();
+    //     });
+    // });
+    MongoClient.connect(url, function(err, db) {
         if (err) throw err;
-        var dbo = client.db("mydb");
-        dbo.collection("chords").find({}, { projection: { _id: 0, name: 1, artis: 2, album: 3, type: 4, link: 5 } }).toArray(function (err, result) {
-            if (err) throw err;
-            result.forEach(function (result) {
-                json['Sheet1'].push(result);
-            });
-            res.json(json);
-            client.close();
-        });
-    });
+        console.log("Database created!");
+        db.close();
+      });
 };
 
 exports.add_a_thing = function (req, res) {
@@ -82,26 +88,26 @@ exports.delete_a_thing = function (req, res) {
 
         if (mName == ('undefined')) { console.log("Enter no name"); }
         else {
-           // console.log("Enter name");
+            // console.log("Enter name");
             mQuery = { name: mName };
         }
         if (mArtis == ('undefined')) { console.log("Enter no artis"); }
         else {
-           // console.log("Enter artis");
+            // console.log("Enter artis");
             mQuery = { name: mName, artis: mArtis };
         }
 
         if (mAlbum == ('undefined')) { console.log("Enter no album"); }
         else {
-          //  console.log("Enter album");
+            //  console.log("Enter album");
             mQuery = { name: mName, artis: mArtis, album: mAlbum };
         }
         if (mType == ('undefined')) { console.log("Enter no type"); } else {
-           // console.log("Enter type");
+            // console.log("Enter type");
             mQuery = { name: mName, artis: mArtis, album: mAlbum, type: mType };
         }
         if (mLink == ('undefined')) { console.log("Enter no link"); } else {
-           // console.log("Enter link");
+            // console.log("Enter link");
             mQuery = { name: mName, artis: mArtis, album: mAlbum, type: mType, link: mLink };
         }
         console.log(mQuery);
