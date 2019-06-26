153621cbb60da4ee708c42cfcca01c37f804bd52
Wayyan <46549417+Wayyan@users.noreply.github.com>
2019-06-26 09:26:12 +0630

    Add

--- index.js
+++
@@ -1,6 +0,0 @@
-const http=require('http');
-const fs=require('fs');
-const express=require('express'),app=express();
-const mRoutes=require('./api/controllers/todoRoutes');
-mRoutes(app);
-app.listen(process.env.PORT||'3002');
